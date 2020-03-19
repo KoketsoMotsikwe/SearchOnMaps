d845bb3e96dd72c45d13eeb4ebc69233d08dbddc
Koketso <koketsomotsikwe@icloud.com>
2020-03-19 11:54:07 +0200

    Initial Commit

---
+++ FindMallsOnMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindMallsOnMap
+//
+//  Created by Koketso Motsikwe (ZA) on 2020/03/19.
+//  Copyright © 2020 Koketso Motsikwe (ZA). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FindMallsOnMapTests/FindMallsOnMapTests.swift
@@ -0,0 +1,34 @@
+//
+//  FindMallsOnMapTests.swift
+//  FindMallsOnMapTests
+//
+//  Created by Koketso Motsikwe (ZA) on 2020/03/19.
+//  Copyright © 2020 Koketso Motsikwe (ZA). All rights reserved.
+//
+
+import XCTest
+@testable import FindMallsOnMap
+
+class FindMallsOnMapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
